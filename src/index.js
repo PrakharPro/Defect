4541d17d41a58f59558f3c75f6943616110ff5fc
PrakharPro <prakharrajput112@gmail.com>
2023-09-16 12:54:40 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const {firebaseapp} = require('./firebase.js');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!');
     });
 
     app.listen(port, () => {
